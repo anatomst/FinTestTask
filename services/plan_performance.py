0d0061f3405fb7758aa0ca46a0ce0b2e7e3f07c6
anatomst <www.shpachyk98@gmail.com>
2023-03-19 22:04:39 +0200

    added get_sum function

--- services/plan_performance.py
+++ services/plan_performance.py
@@ -3,7 +3,7 @@ from datetime import date, datetime, time
 from sqlalchemy import and_, func, extract
 from sqlalchemy.orm import Session
 
-from models.models import Credit, Payment, Plan
+from models.models import Credit, Payment, Plan, Base
 
 
 def get_plan_performance_service(check_date: date, db: Session):
@@ -18,10 +18,7 @@ def get_plan_performance_service(check_date: date, db: Session):
 
     for plan in plans:
         if plan.category.name == "видача":
-            credit_sum = db.query(Credit).filter(and_(
-                Credit.issuance_date >= datetime.combine(plan.period.replace(day=1), time.min),
-                Credit.issuance_date <= datetime.combine(check_date, time.max))
-            ).with_entities(func.sum(Credit.body)).scalar() or 0
+            credit_sum = get_sum(Credit, "body", plan.period.replace(day=1), check_date, db)
 
             plan_performances.append({"month": plan.period,
                                       "category": plan.category.name,
@@ -30,10 +27,7 @@ def get_plan_performance_service(check_date: date, db: Session):
                                       "percent": round(credit_sum / plan.sum * 100, 2) if credit_sum else 0})
 
         if plan.category.name == "збір":
-            payments_sum = db.query(Payment).filter(and_(
-                Payment.payment_date >= datetime.combine(plan.period.replace(day=1), time.min),
-                Payment.payment_date <= datetime.combine(check_date, time.max))
-            ).with_entities(func.sum(Payment.sum)).scalar() or 0
+            payments_sum = get_sum(Payment, "sum", plan.period.replace(day=1), check_date, db)
 
             plan_performances.append({"month": plan.period,
                                       "category": plan.category.name,
@@ -42,3 +36,14 @@ def get_plan_performance_service(check_date: date, db: Session):
                                       "percent": round(payments_sum / plan.sum * 100, 2) if payments_sum else 0})
 
     return plan_performances
+
+
+def get_sum(table: Base, sum_column: str, first_day: date, check_date: date, db: Session):
+    """
+    Calculates the amount of loans issued or the amount of payments for
+    the period from the beginning of the plan month to the received date
+    """
+    return db.query(table).filter(and_(
+        Payment.payment_date >= datetime.combine(first_day, time.min),
+        Payment.payment_date <= datetime.combine(check_date, time.max))
+    ).with_entities(func.sum(getattr(table, sum_column))).scalar() or 0
